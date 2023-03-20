9ba193bd48a27145887aeb37b57f97a323e41072
Ivan-Stancic <ivanstancic983@gmail.com>
2023-03-20 15:00:04 +0100

    seminar dorada

--- chat-app/src/components/login/Login.js
+++ chat-app/src/components/login/Login.js
@@ -15,19 +15,19 @@ function Login({ userEnter }) {
 
   return (
     <div className = "background">
-    <Box className="enter-page">
-        <TextField label="Odaberi Nickname" color="primary"
-          required
-          id="input-nickname"
-          variant="outlined"
-          type="text"
-          value={nickname}
-          onChange={handleInputChange}
-        />
-        <Button variant="contained" color="primary" onClick={handleSubmit}>
-          Ulazak u sobu
-        </Button>
-    </Box>
+      <Box className="enter-page">
+          <TextField label="Odaberi Nickname" color="primary"
+            required
+            id="input-nickname"
+            variant="outlined"
+            type="text"
+            value={nickname}
+            onChange={handleInputChange}
+          />
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Ulazak u sobu
+          </Button>
+      </Box>
     </div>
   );
 }
